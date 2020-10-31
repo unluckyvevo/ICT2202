19265c510ebde4ce9ae9fca7d6fbe58e2c2e5653
unluckyvevo <unluckyvevo@gmail.com>
2020-11-01 03:20:02 +0800

    This should be the almost final update

--- ForensicVer2.py
+++ ForensicVer2.py
@@ -31,8 +31,10 @@ filenameCSV=None
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        font = QtGui.QFont()
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 604)
+        MainWindow.setStyleSheet("background-color:rgb(60, 110, 113)")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
@@ -40,52 +42,69 @@ class Ui_MainWindow(object):
         self.tabWidget.setObjectName("tabWidget")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
+        
         self.lbl_Address = QtWidgets.QLabel(self.tab_2)
         self.lbl_Address.setGeometry(QtCore.QRect(30, 30, 81, 20))
         self.lbl_Address.setObjectName("lbl_Address")
+        
         self.btn_CheckscamScore = QtWidgets.QPushButton(self.tab_2)
         self.btn_CheckscamScore.setGeometry(QtCore.QRect(270, 370, 75, 23))
         self.btn_CheckscamScore.setObjectName("btn_CheckscamScore")
         self.btn_CheckscamScore.clicked.connect(self.sentScamCheck)
+        
         self.lineEdit_Address = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_Address.setGeometry(QtCore.QRect(140, 30, 131, 20))
         self.lineEdit_Address.setObjectName("lineEdit_Address")
+        
         self.lbl_Postal = QtWidgets.QLabel(self.tab_2)
         self.lbl_Postal.setGeometry(QtCore.QRect(30, 80, 81, 20))
         self.lbl_Postal.setObjectName("lbl_Postal")
-        self.lbl_State = QtWidgets.QLabel(self.tab_2)
+        
+        self.lbl_State = QtWidgets.QLabel(self.tab_2) 
         self.lbl_State.setGeometry(QtCore.QRect(30, 120, 81, 20))
         self.lbl_State.setObjectName("lbl_State")
         self.lbl_Email = QtWidgets.QLabel(self.tab_2)
         self.lbl_Email.setGeometry(QtCore.QRect(30, 160, 81, 20))
         self.lbl_Email.setObjectName("lbl_Email")
-        self.lbl_Email_2 = QtWidgets.QLabel(self.tab_2)
-        self.lbl_Email_2.setGeometry(QtCore.QRect(30, 210, 81, 20))
-        self.lbl_Email_2.setObjectName("lbl_Email_2")
-        self.lbl_Email_3 = QtWidgets.QLabel(self.tab_2)
-        self.lbl_Email_3.setGeometry(QtCore.QRect(30, 260, 81, 20))
-        self.lbl_Email_3.setObjectName("lbl_Email_3")
-        self.lbl_Email_4 = QtWidgets.QLabel(self.tab_2)
-        self.lbl_Email_4.setGeometry(QtCore.QRect(30, 310, 81, 20))
-        self.lbl_Email_4.setObjectName("lbl_Email_4")
+        self.lbl_IPAddress = QtWidgets.QLabel(self.tab_2)
+        self.lbl_IPAddress.setGeometry(QtCore.QRect(30, 210, 81, 20))
+        self.lbl_IPAddress.setObjectName("lbl_IPAddress")
+        self.lbl_PhoneNumber = QtWidgets.QLabel(self.tab_2)
+        self.lbl_PhoneNumber.setGeometry(QtCore.QRect(30, 260, 81, 20))
+        self.lbl_PhoneNumber.setObjectName("lbl_PhoneNumber")
+        self.lbl_UserAgent = QtWidgets.QLabel(self.tab_2)
+        self.lbl_UserAgent.setGeometry(QtCore.QRect(30, 310, 81, 20))
+        self.lbl_UserAgent.setObjectName("lbl_UserAgent")
+        
         self.lineEdit_Postal = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_Postal.setGeometry(QtCore.QRect(140, 80, 131, 20))
         self.lineEdit_Postal.setObjectName("lineEdit_Postal")
+        
         self.lineEdit_State = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_State.setGeometry(QtCore.QRect(140, 120, 131, 20))
         self.lineEdit_State.setObjectName("lineEdit_State")
+        
         self.lineEdit_Email = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_Email.setGeometry(QtCore.QRect(140, 160, 131, 20))
         self.lineEdit_Email.setObjectName("lineEdit_Email")
+        
         self.lineEdit_IP_Address = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_IP_Address.setGeometry(QtCore.QRect(140, 210, 131, 20))
         self.lineEdit_IP_Address.setObjectName("lineEdit_IP_Address")
+        
+        
         self.lineEdit_PhoneNumber = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_PhoneNumber.setGeometry(QtCore.QRect(140, 260, 131, 20))
         self.lineEdit_PhoneNumber.setObjectName("lineEdit_PhoneNumber")
+        
         self.lineEdit_UserAgent = QtWidgets.QLineEdit(self.tab_2)
         self.lineEdit_UserAgent.setGeometry(QtCore.QRect(140, 310, 131, 20))
         self.lineEdit_UserAgent.setObjectName("lineEdit_UserAgent")
+        
+        self.lbl_FraudScore = QtWidgets.QLabel(self.tab_2)
+        self.lbl_FraudScore.setGeometry(QtCore.QRect(40, 440, 151, 16))
+        self.lbl_FraudScore.setObjectName("lbl_FraudScore")
+        
         self.tabWidget.addTab(self.tab_2, "")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
@@ -104,7 +123,11 @@ class Ui_MainWindow(object):
         self.file_path_line.setObjectName("file_path_line")
         
         self.lbl_examine = QtWidgets.QLabel(self.tab)
-        self.lbl_examine.setGeometry(QtCore.QRect(330, 10, 101, 16))
+        self.lbl_examine.setGeometry(QtCore.QRect(330, 0, 121, 21))
+        
+        font.setFamily("Garamond")
+        font.setPointSize(12)
+        self.lbl_examine.setFont(font)
         self.lbl_examine.setObjectName("lbl_examine")
         
         self.Submit_btn = QtWidgets.QPushButton(self.tab)
@@ -112,20 +135,25 @@ class Ui_MainWindow(object):
         self.Submit_btn.setObjectName("Submit_btn")
         self.Submit_btn.clicked.connect(self.submitPCAP)
         
-        self.Save_to_csv = QtWidgets.QPushButton(self.tab)
-        self.Save_to_csv.setGeometry(QtCore.QRect(580, 520, 75, 23))
-        self.Save_to_csv.setObjectName("Save_to_csv")
         
-        self.checkBox_dest = QtWidgets.QCheckBox(self.tab)
-        self.checkBox_dest.setGeometry(QtCore.QRect(450, 80, 81, 17))
-        self.checkBox_dest.setObjectName("checkBox_dest")
+        self.lbl_Destination = QtWidgets.QLabel(self.tab)
+        self.lbl_Destination.setGeometry(QtCore.QRect(430, 80, 101, 16))
         
-        self.checkBox_src = QtWidgets.QCheckBox(self.tab)
-        self.checkBox_src.setGeometry(QtCore.QRect(250, 80, 70, 17))
-        self.checkBox_src.setObjectName("checkBox_src")
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self.lbl_Destination.setFont(font)
+        self.lbl_Destination.setObjectName("lbl_Destination")
+        
+        self.lbl_Source = QtWidgets.QLabel(self.tab)
+        self.lbl_Source.setGeometry(QtCore.QRect(250, 80, 61, 16))
+        
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self.lbl_Source.setFont(font)
+        self.lbl_Source.setObjectName("lbl_Source")
         
         self.checkBox_SinglePkt = QtWidgets.QCheckBox(self.tab)
-        self.checkBox_SinglePkt.setGeometry(QtCore.QRect(260, 120, 131, 17))
+        self.checkBox_SinglePkt.setGeometry(QtCore.QRect(260, 130, 131, 17))
         self.checkBox_SinglePkt.setObjectName("checkBox_SinglePkt")
         
         self.textBrowser_PCAP = QtWidgets.QTextBrowser(self.tab)
@@ -133,7 +161,7 @@ class Ui_MainWindow(object):
         self.textBrowser_PCAP.setObjectName("textBrowser_PCAP")
         
         self.lineEdit_singlePacketNo = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit_singlePacketNo.setGeometry(QtCore.QRect(390, 120, 71, 16))
+        self.lineEdit_singlePacketNo.setGeometry(QtCore.QRect(380, 130, 71, 16))
         self.lineEdit_singlePacketNo.setObjectName("lineEdit_singlePacketNo")
         
         self.line = QtWidgets.QFrame(self.tab)
@@ -144,6 +172,9 @@ class Ui_MainWindow(object):
         
         self.checkBox_4 = QtWidgets.QCheckBox(self.tab)
         self.checkBox_4.setGeometry(QtCore.QRect(310, 50, 201, 17))
+        font.setFamily("Garamond")
+        font.setPointSize(12)
+        self.checkBox_4.setFont(font)
         self.checkBox_4.setObjectName("checkBox_4")
         
         self.lineEdit_SourceIP = QtWidgets.QLineEdit(self.tab)
@@ -155,23 +186,29 @@ class Ui_MainWindow(object):
         self.lineEdit_DestIP.setObjectName("lineEdit_DestIP")
         
         self.label_IsVerbose = QtWidgets.QLabel(self.tab)
-        self.label_IsVerbose.setGeometry(QtCore.QRect(150, 140, 91, 16))
+        self.label_IsVerbose.setGeometry(QtCore.QRect(170, 160, 81, 20))
+        font.setFamily("Century Gothic")
+        self.label_IsVerbose.setFont(font)
         self.label_IsVerbose.setObjectName("label_IsVerbose")
         
         self.radioButton_Yes = QtWidgets.QRadioButton(self.tab)
-        self.radioButton_Yes.setGeometry(QtCore.QRect(300, 140, 82, 17))
+        self.radioButton_Yes.setGeometry(QtCore.QRect(300, 160, 82, 17))
         self.radioButton_Yes.setObjectName("radioButton_Yes")
+        self.radioButton_Yes.setChecked(True)
         
         self.radioButton_No = QtWidgets.QRadioButton(self.tab)
-        self.radioButton_No.setGeometry(QtCore.QRect(430, 140, 82, 17))
+        self.radioButton_No.setGeometry(QtCore.QRect(430, 160, 82, 17))
         self.radioButton_No.setObjectName("radioButton_No")
         
         self.radioButton_raw = QtWidgets.QRadioButton(self.tab)
-        self.radioButton_raw.setGeometry(QtCore.QRect(510, 140, 82, 17))
+        self.radioButton_raw.setGeometry(QtCore.QRect(510, 160, 82, 17))
         self.radioButton_raw.setObjectName("radioButton_raw")
         
         self.checkBox_SrcDest = QtWidgets.QCheckBox(self.tab)
-        self.checkBox_SrcDest.setGeometry(QtCore.QRect(110, 80, 121, 17))
+        self.checkBox_SrcDest.setGeometry(QtCore.QRect(100, 80, 141, 20))
+        font.setFamily("Century Gothic")
+        font.setPointSize(8)
+        self.checkBox_SrcDest.setFont(font)
         self.checkBox_SrcDest.setObjectName("checkBox_SrcDest")
         
         self.tabWidget.addTab(self.tab, "")
@@ -189,6 +226,16 @@ class Ui_MainWindow(object):
         self.btn_ShowReport.setGeometry(QtCore.QRect(260, 110, 75, 23))
         self.btn_ShowReport.setObjectName("btn_ShowReport")
         self.btn_ShowReport.clicked.connect(self.displayChart)
+        
+        self.lblCreditCardFraudGeneric = QtWidgets.QLabel(self.tab_3)
+        self.lblCreditCardFraudGeneric.setGeometry(QtCore.QRect(110, 20, 421, 20))
+        font.setFamily("Bodoni MT Black")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.lblCreditCardFraudGeneric.setFont(font)
+        self.lblCreditCardFraudGeneric.setObjectName("lblCreditCardFraudGeneric")
+        
         
         self.file_path_line_2 = QtWidgets.QLineEdit(self.tab_3)
         self.file_path_line_2.setGeometry(QtCore.QRect(170, 70, 211, 20))
@@ -215,9 +262,15 @@ class Ui_MainWindow(object):
         self.webEngineView.setObjectName("webEngineView")
         
         
-
+        
         self.lbl_Amazon = QtWidgets.QLabel(self.tab_4)
-        self.lbl_Amazon.setGeometry(QtCore.QRect(200, 40, 251, 21))
+        self.lbl_Amazon.setGeometry(QtCore.QRect(30, 10, 531, 41))
+        font = QtGui.QFont()
+        font.setFamily("Bodoni MT Black")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.lbl_Amazon.setFont(font)
         self.lbl_Amazon.setObjectName("lbl_Amazon")
        
         
@@ -237,6 +290,27 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(2)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     def pcapFilePath(self):
         pcap_path=QtWidgets.QFileDialog.getOpenFileName(None, 'Single File', '', '*.pcap')
@@ -306,18 +380,7 @@ class Ui_MainWindow(object):
              self.textBrowser_PCAP.append(str(test[i]))
   
         
-    def on_browse(self):
-       filename=QtWidgets.QFileDialog.getOpenFileName(None, 'Single File', '', '*.txt')
-       print(filename)
-       self.file_path_line.setText(filename[0])
-       
-       df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
-
-                   'mask': ['red', 'purple'],
-
-                   'weapon': ['sai', 'bo staff']})
-       df.to_csv('Test.txt',index=False) # saves the DataFrame to a text file
-       self.dataframe_label.setText(df.to_string())
+   
        
        
        
@@ -338,8 +401,8 @@ class Ui_MainWindow(object):
          
          self.textBrowser_2.setText(getGraph[0])
          self.textBrowser_2.append(getGraph[1])
-         self.textBrowser_2.append(getGraph[2])
-         for x in range(3,len(getGraph)):
+         
+         for x in range(2,len(getGraph)):
              self.textBrowser_2.append('<img src="%s" />' %getGraph[x])
              #self.textBrowser_2.append('<img src="%s" />' %getGraph[3])
          
@@ -378,11 +441,22 @@ class Ui_MainWindow(object):
          with open('result.json', 'w') as f:
             print(result, file=f)
          f = open("result.json", "r")
+         contents=f.read()
+         contents = contents.replace("'", '"')
+         with open('result.json', 'w') as file:
+             file.write(contents)
          
-         f = f.replace("'", '"')
-         jsonList = json.load(f)
-         faggot=jsonList['scores']
-         print(faggot)
+         
+         with open('result.json') as f:
+             data = json.load(f)
+        
+         for modelScores in data["modelScores"]:
+             diction=modelScores.get('scores')
+
+         for key, value in diction.items():
+            vl=value
+         self.lbl_FraudScore.setText("Fraud Score: "+str(vl))
+        
                  
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -392,16 +466,16 @@ class Ui_MainWindow(object):
         self.lbl_Postal.setText(_translate("MainWindow", "Postal:"))
         self.lbl_State.setText(_translate("MainWindow", "State:"))
         self.lbl_Email.setText(_translate("MainWindow", "Email:"))
-        self.lbl_Email_2.setText(_translate("MainWindow", "IP Address"))
-        self.lbl_Email_3.setText(_translate("MainWindow", "Phone number"))
-        self.lbl_Email_4.setText(_translate("MainWindow", "User agent"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+        self.lbl_IPAddress.setText(_translate("MainWindow", "IP Address"))
+        self.lbl_PhoneNumber.setText(_translate("MainWindow", "Phone number"))
+        self.lbl_UserAgent.setText(_translate("MainWindow", "User agent"))
+        self.lbl_FraudScore.setText(_translate("MainWindow", "Fraud Score:"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Contact Fraud Detector"))
         self.browse_button.setText(_translate("MainWindow", "Browse...."))
         self.lbl_examine.setText(_translate("MainWindow", "Examine PCAP"))
         self.Submit_btn.setText(_translate("MainWindow", "Submit"))
-        self.Save_to_csv.setText(_translate("MainWindow", "Save to CSV"))
-        self.checkBox_dest.setText(_translate("MainWindow", "Destination"))
-        self.checkBox_src.setText(_translate("MainWindow", "Source"))
+        self.lbl_Destination.setText(_translate("MainWindow", "Destination:"))
+        self.lbl_Source.setText(_translate("MainWindow", "Source"))
         self.checkBox_SinglePkt.setText(_translate("MainWindow", "Show a single packet"))
         self.checkBox_4.setText(_translate("MainWindow", "Show PCAP Summary"))
         self.label_IsVerbose.setText(_translate("MainWindow", "Is Verbose?"))
@@ -412,7 +486,8 @@ class Ui_MainWindow(object):
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Pcap Analyse"))
         self.browseCSVbtn.setText(_translate("MainWindow", "Browse..."))
         self.btn_ShowReport.setText(_translate("MainWindow", "Submit"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Fraud Detection"))
+        self.lblCreditCardFraudGeneric.setText(_translate("MainWindow", "Credit Card Fraud Detector"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Credit Card Fraud Detection"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Website "))
         self.lbl_Amazon.setText(_translate("MainWindow", "Amazon Credit Card Face Recognition"))
 if __name__ == "__main__":
